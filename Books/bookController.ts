9f207724c21db4387115bcec863b1bfe79457e8a
zakirkhan-code <zakirullah35202@gmail.com>
2024-10-25 15:50:47 +0500

    upload Image and Pdf i cloudinary

--- Books/bookController.ts
+++ Books/bookController.ts
@@ -5,11 +5,8 @@ import cloudinary from "../config/cloudinary";
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
     console.log("Files", req.files);
     const file = req.files as { [fieldname: string]: Express.Multer.File[] };
-
     const coverImageMimeTypes = file.coverImage[0].mimetype.split('/').at(-1);
-
     const fileName = file.coverImage[0].filename; 
-
     const filePath = path.resolve(__dirname, "../../public/data/uploads", fileName);
 
     try {
@@ -20,11 +17,30 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
         });
 
         console.log("Upload result", uploadResult);
-        res.json({ message: "OK", uploadResult });
+        res.json({ message: "IMAGE IS OK", uploadResult });
     } catch (error) {
-        console.error("Cloudinary upload error:", error);
-        return next(new Error("Failed to upload image to Cloudinary"));
+        console.error("Cloudinary upload Image error:", error);
+        return next(new Error("Failed to upload Image to Cloudinary"));
     }
+
+
+    const BookName = file.file[0].filename;
+    const BookPath = path.resolve(__dirname,"../../public/data/uploads",BookName);
+
+    try {
+        const uploadBooks = await cloudinary.uploader.upload(BookPath,{
+            resource_type:"raw",
+            filename_override: BookName,
+            folder: "Book-Pdf",
+            format:"pdf"
+        })
+        console.log("Upload Book PDF", uploadBooks);
+        res.json({ message: "Book PDF IS OK", uploadBooks });
+    } catch (error) {
+        console.error("Cloudinary upload BOOK PDF error:", error);
+        return next(new Error("Failed to upload BOOK PDF to Cloudinary"));
+    }
+
 };
 
 export { createBook };
